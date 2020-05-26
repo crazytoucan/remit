6ba73764c465ddb27500325cc4fe5ac86c5f59f4
Alex Ryan <alexanderfryan@gmail.com>
2020-05-26 11:39:27 -0700

    Default defineAction() generic arg to undefined (#6)

--- src/functions.ts
+++ src/functions.ts
@@ -1,6 +1,6 @@
 import { IActionDefinition, IActionType, IEmitter } from "./types";
 
-export function defineAction<T>(type: string) {
+export function defineAction<T = undefined>(type: string) {
   const definition: IActionDefinition<T> = ((payload: T) => ({ type, payload })) as any;
   definition.TYPE = type as IActionType<T>;
   return definition;
