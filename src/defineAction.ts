0e95fa50c9ea47a1a9d236727af7297d1e3f4a75
Alex Ryan <alexanderfryan@gmail.com>
2020-05-27 17:08:56 -0700

    Change defineAction() syntax to match similar functions from other OSS libraries

--- src/defineAction.ts
+++ src/defineAction.ts
@@ -2,11 +2,21 @@ import { IActionDefinition, IActionType } from "./types";
 
 /**
  * Defines a new Action type for typesafe action handling.
+ * The syntax is a little tricky, but wsa made this way to
+ * prevent spelling errors in action types.
+ *
+ * Example usage:
+ *
+ * ```
+ * const LoadUser = defineAction("LoadUser")<{ userId: string }>();
+ * ```
  *
  * @param type the Action type string being defined
  */
-export function defineAction<T = undefined>(type: string) {
-  const definition: IActionDefinition<T> = ((payload: T) => ({ type, payload })) as any;
-  definition.TYPE = type as IActionType<T>;
-  return definition;
+export function defineAction(type: string): <T = undefined>() => IActionDefinition<T> {
+  return <T = undefined>() => {
+    const definition: IActionDefinition<T> = ((payload: T) => ({ type, payload })) as any;
+    definition.TYPE = type as IActionType<T>;
+    return definition;
+  };
 }
