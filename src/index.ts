cefdaf1447ac9e378f497756262339287ff4fd04
Alex Ryan <alexanderfryan@gmail.com>
2020-06-09 17:46:38 -0700

    Add takeEvery() effect for similarity to redux-saga (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { defineAction } from "./defineAction";
-export { on, once, put, take } from "./effects";
+export { on, once, put, take, takeEvery } from "./effects";
 export { Emitter } from "./Emitter";
 export { Store } from "./Store";
 export { IAction, IActionDefinition, IActionType, IEmitter, IStore } from "./types";
