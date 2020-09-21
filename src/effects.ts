c9e8b548377f21c0feb5ae0747b4a546e53fac69
Alex Ryan <alexanderfryan@gmail.com>
2020-09-21 18:54:38 -0400

    Remove unnecessary effects (#14)

--- src/effects.ts
+++ src/effects.ts
@@ -1,124 +1,65 @@
-import { IAction, IActionType, IChannel } from "./types";
-
-const TRUE_PREDICATE = () => true;
-
-/**
- * Equivalent to `emitter.on()`, exported as an Effect for consistency.
- *
- * @param emitter the emitter to hook into
- * @param type the action type to listen on
- * @param cb a callback called whenever the action is emitted
- */
-export function on<T>(emitter: IChannel, type: IActionType<T>, cb: (t: T) => void) {
-  return emitter.on(type, cb);
-}
-
-/**
- * Listens for `emitter.on()`, invokes the callback exactly once, and then unlistens.
- * Returns a handle which, when called, cancels this effect.
- *
- * @param emitter the emitter to hook into
- * @param type the action type to listen on
- * @param cb a callback called exactly once when the next action is emitted
- */
-export function once<T>(emitter: IChannel, type: IActionType<T>, cb: (t: T) => void) {
-  const destroy = emitter.on(type, (value: T) => {
-    destroy();
-    cb(value);
-  });
-
-  return destroy;
-}
+import { IActionType, IChannel } from "./types";
 
 /**
  * Returns a promise whose resolved value will be the next action of the given type
- * emitted on the emitter. If the `maxWait` parameter is specified, then at most
+ * emitted on the channel. If the `maxWait` parameter is specified, then at most
  * `maxWait` milliseconds will be waited before returning the default value `null`.
  *
- * @param emitter emitter to hook into
+ * @param channel channel to hook into
  * @param type the action type to listen on
  */
-export function take<T>(emitter: IChannel, type: IActionType<T>): Promise<T>;
-
-/**
- * Returns a promise whose resolved value will be the next action of the given type
- * emitted on the emitter. If the `maxWait` parameter is specified, then at most
- * `maxWait` milliseconds will be waited before returning the default value `null`.
- *
- * @param emitter emitter to hook into
- * @param type the action type to listen on
- * @param maxWait the maximum time, in millis, to wait for the given action type
- */
-export function take<T>(
-  emitter: IChannel,
-  type: IActionType<T>,
-  maxWait: number,
-): Promise<T | null>;
-
-/**
- * Returns a promise whose resolved value will be the next action of the given type
- * emitted on the emitter. If the `maxWait` parameter is specified, then at most
- * `maxWait` milliseconds will be waited before returning the default value `null`.
- *
- * @param emitter emitter to hook into
- * @param type the action type to listen on
- * @param maxWait the maximum time, in millis, to wait for the given action type
- */
-export function take<T>(
-  emitter: IChannel,
-  type: IActionType<T>,
-  filter: (payload: T) => boolean,
-  maxWait: number,
-): Promise<T | null>;
-
-export function take<T>(
-  emitter: IChannel,
-  type: IActionType<T>,
-  arg2?: number | Function, // tslint:disable-line:ban-types
-  arg3?: number,
-) {
-  const filter = typeof arg2 === "function" ? arg2 : TRUE_PREDICATE;
-  const maxWait = typeof arg2 === "number" ? arg2 : arg3;
-
-  return new Promise<T | null>((resolve) => {
-    const unsubscribe = emitter.on(type, (value: T) => {
-      if (filter(value)) {
-        if (timeout !== 0) {
-          clearTimeout(timeout);
-        }
-
-        resolve(value);
-      }
+export function take<T>(channel: IChannel, type: IActionType<T>): Promise<T> {
+  return new Promise<T>((resolve) => {
+    const unsubscribe = channel.on(type, (value: T) => {
+      unsubscribe();
+      resolve(value);
     });
-
-    const timeout =
-      maxWait !== undefined
-        ? setTimeout(() => {
-            unsubscribe();
-            resolve(null);
-          }, maxWait)
-        : 0;
   });
 }
 
 /**
- * Equivalent to `emitter.on()`, exported as an Effect for consistency.
+ * Returns a promise whose resolved value will be the next action of the given type
+ * emitted on the channel. If the `maxWait` parameter is specified, then at most
+ * `maxWait` milliseconds will be waited before returning the default value `null`.
  *
- * @param emitter the emitter to hook into
+ * @param channel channel to hook into
  * @param type the action type to listen on
- * @param cb a callback called whenever the action is emitted
+ * @param maxWait the maximum time, in millis, to wait for the given action type
  */
-export function takeEvery<T>(emitter: IChannel, type: IActionType<T>, cb: (t: T) => void) {
-  return emitter.on(type, cb);
-}
+export function takeAdvanced<T>(
+  channel: IChannel,
+  type: IActionType<T>,
+  opts: {
+    filter?: (payload: T) => boolean;
+    maxWait?: number;
+  },
+): Promise<T | null> {
+  const filter = opts.filter ?? (() => true);
+  const maxWait = opts.maxWait;
 
-/**
- *
- * Equivalent to `emitter.put()`, exported as an Effect for consistency.
- *
- * @param emitter the emitter to use
- * @param action the action to emit
- */
-export function put(emitter: IChannel, action: IAction) {
-  emitter.put(action);
+  if (maxWait !== undefined) {
+    return new Promise<T>((resolve) => {
+      const unsubscribe = channel.on(type, (value: T) => {
+        if (filter(value)) {
+          unsubscribe();
+          resolve(value);
+        }
+      });
+    });
+  } else {
+    return new Promise<T | null>((resolve) => {
+      const timeout = setTimeout(() => {
+        unsubscribe();
+        resolve(null);
+      });
+
+      const unsubscribe = channel.on(type, (value: T) => {
+        if (filter(value)) {
+          clearTimeout(timeout);
+          unsubscribe();
+          resolve(value);
+        }
+      });
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { on, once, put, take, takeEvery } from "./effects";
 export { Channel } from "./Channel";
+export { take, takeAdvanced } from "./effects";
 export { Store } from "./Store";
 export { IAction, IActionType, IChannel, IStore } from "./types";
