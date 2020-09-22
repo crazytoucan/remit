6e1e6092735b6e958cb64a1f74318f0b42c6d26d
Alex Ryan <alexanderfryan@gmail.com>
2020-09-21 23:23:50 -0400

    Add createAction() (#18)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Channel } from "./Channel";
 export { TINYSAGA_CONTEXT } from "./context";
+export { createAction } from "./createAction";
 export { take } from "./effects/take";
 export { takeAdvanced } from "./effects/takeAdvanced";
 export { createUseDispatch, useDispatch } from "./hooks/useDispatch";
